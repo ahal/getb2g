01d4de5c40022e78b39a8c3013fd4d91c16b5851
Andrew Halberstadt <halbersa@gmail.com>
2012-10-04 09:58:01 -0400

    Clean up repo

--- reftest/htmlify.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def sorted_ls(path):
-    mtime = lambda f: os.stat(os.path.join(path, f)).st_mtime
-    return list(sorted(os.listdir(path), key=mtime))
-
-for f in sorted_ls('.'):
-    os.system('perl reftest-to-html.pl ' + f + ' > html/' + f.replace('log', 'html'))
-
