f91c63970e64b9bd6c1d3e2a180971d98a7317e3
Andrew Halberstadt <halbersa@gmail.com>
2013-07-12 11:27:06 -0400

    Prompt user to save password if --store wasn't specified

--- getb2g/mixins/localstorage.py
+++ getb2g/mixins/localstorage.py
@@ -2,14 +2,17 @@ import base64
 import os
 import sqlite3
 
+from ..prompt import prompt
+
 __all__ = ('StorageMixin',)
 
 class StorageMixin(object):
-    db = os.path.expanduser(os.path.join('~', '.getb2g', 'storage.db'))
+    storage_dir = os.path.expanduser(os.path.join('~', '.getb2g'))
+    db = os.path.join(storage_dir, 'storage.db')
 
     def __init__(self, **kwargs):
-        if not os.path.isdir(os.path.dirname(self.db)):
-            os.makedirs(os.path.dirname(self.db))
+        if not os.path.isdir(self.storage_dir):
+            os.makedirs(self.storage_dir)
         self.conn = sqlite3.connect(self.db)
         self.cur = self.conn.cursor()
         self.cur.execute('create table if not exists passwords (domain, user, passwd)')
@@ -20,7 +23,15 @@ class StorageMixin(object):
 
     def save_auth(self, url, user, password):
         if not self.metadata['store']:
-            return
+            if os.path.isfile(os.path.join(self.storage_dir, 'dont')):
+                return
+            response = prompt("Save this password in plain text for later?", valid_answers=('y', 'n', 'never'))
+            if response == 'never':
+                with open(os.path.join(self.storage_dir, 'dont'), 'w') as f:
+                    pass
+                return
+            elif response == 'n':
+                return
         self.cur.execute('insert into passwords values (?, ?, ?)', (url, user, base64.b64encode(password)))
 
     def load_auth(self, url):
